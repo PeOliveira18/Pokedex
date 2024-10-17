a76c3a44d5094d274e232a1901c507fbb889cf45
Pedro Oliveira <oliveirasilvapedro2@gmail.com>
2024-10-17 11:20:41 -0300

    pokedex

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+
+export const AppContainer = styled.div`
+  margin: 0;
+  padding: 0;
+  max-width: 100%;
+`
+
+function App() {
+
+  return (
+    <AppContainer>
+      
+    </AppContainer>
+  )
+}
+
+export default App
